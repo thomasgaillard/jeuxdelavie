235eb00fb2426accf2958850ab1faa68043cac40
thomasgaillard <thomas.gaillard@me.com>
2012-06-01 10:28:18 +0200

    observer+fenetreprinc

--- src/viewController/Case.java
+++ src/viewController/Case.java
@@ -1,19 +1,10 @@
 package viewController;
 
 import java.awt.Color;
-import java.util.Random;
 import javax.swing.JPanel;
 
 public class Case extends JPanel {
 	private boolean etatCellule;
-
-	public boolean getEtatCellule() {
-		return etatCellule;
-	}
-
-	public void setEtatCellule(boolean etatCellule) {
-		this.etatCellule = etatCellule;
-	}
 
 	public Case() {
 		super();
@@ -24,14 +15,10 @@ public class Case extends JPanel {
 		this.etatCellule = etatCellule;
 	}
 	
-	public void changeEtat(){
-		Random rand=new Random();
-		this.etatCellule=rand.nextBoolean();
-		if(this.etatCellule)
+	public void changeEtat(boolean etatCellule){
+		if(etatCellule)
 			this.setBackground(Color.BLACK);
 		else
 			this.setBackground(Color.WHITE);
-
 	}
-
 }
--- src/viewController/FenetrePrincipale.java
+++ src/viewController/FenetrePrincipale.java
@@ -19,46 +19,30 @@ public class FenetrePrincipale extends JFrame {
 	private Modele modele;
 
 	public FenetrePrincipale() {
+		//init
 		n = 25;
 		modele = new Modele(n);
-
+		
+		//fenetre
 		Toolkit k = Toolkit.getDefaultToolkit();
 		Dimension tailleEcran = k.getScreenSize();
 		int hauteurEcran = tailleEcran.height;
 		setTitle("Le jeux de la vie");
 		setSize(hauteurEcran / 2 + 200, hauteurEcran / 2);
 
+		//panels
 		panelPrincipal = new JPanel(new BorderLayout());
 		setContentPane(panelPrincipal);
 		panelDroite = new JPanel();
-
 		panelDroite.setLayout(new GridLayout(3, 1));
+		
+		//buttons
 		JButton init = new JButton("Initialiser");
 		panelDroite.add(init);
-
 		JButton pause = new JButton("Pause");
 		panelDroite.add(pause);
-
 		JButton activ = new JButton("Activité");
 		panelDroite.add(activ);
-
-		init.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				modele.init();
-			}
-		});
-
-		pause.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				modele.pause();
-			}
-		});
-
-		activ.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				modele.activ();
-			}
-		});
 
 		panelCentre = new JPanel();
 
@@ -66,21 +50,47 @@ public class FenetrePrincipale extends JFrame {
 		panelPrincipal.add(panelCentre, BorderLayout.CENTER);
 		panelCentre.setLayout(new GridLayout(n, n));
 
-		this.initialize();
 		this.setResizable(false);
 		this.setVisible(true);
-	}
-
-	public void initialize() {
-		for (int i = 0; i < n * n; i++) {
+		modele.run();
+		
+		//cellules
+		for (int i = 0; i < n*n; i++) {
 			Case cellule = new Case();
-			cellule.changeEtat(modele.tabBool);
 			panelCentre.add(cellule);
 		}
+		
+		//buttons actionListener
+		init.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				modele.init();
+			}
+		});
+		pause.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				modele.pause();
+			}
+		});
+		activ.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				modele.activ();
+			}
+		});
+
+		//
+		public void Notify(){
+			int inc = 0;
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					Case(panelCentre.getComponent(inc)).changeEtat(modele.getTabBool()[i][j]);
+					inc ++;
+				}
+			}
+		}
+		
 	}
 
 	public static void main(String[] args) {
 		FenetrePrincipale test = new FenetrePrincipale();
-
 	}
 }
